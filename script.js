539612903f32139b6767fa82be0b3781286ee4f7
das-Sumyadeep <133164855+das-Sumyadeep@users.noreply.github.com>
2024-05-11 12:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let pq = [];
+
+	for(let x of arr){
+		pq.push(x);
+	}
+	
+	pq.sort();
+
+	let res = 0;
+	
+	while(pq.length > 1){
+
+		let first = pq.shift();
+		let second = pq.shift();
+
+		res += first + second;
+
+		pq.push(first + second);
+
+		pq.sort();
+
+	}
+
+	return res;
   
 }
 
